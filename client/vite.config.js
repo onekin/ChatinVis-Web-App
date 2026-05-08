37ec3ff186c3f822487923de90265fd7ff440958
xabigar <xabier.garmendiad@ehu.eus>
2026-05-08 09:34:42 +0200

    Allow production domain in Vite and CORS configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    strictPort: false
+    strictPort: false,
+    allowedHosts: ['chatinvis.garmendia.eus']
   }
 })
